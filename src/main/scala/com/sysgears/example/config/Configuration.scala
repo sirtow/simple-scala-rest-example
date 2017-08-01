a492b52fbae0f3ecd2977d1da473e1a167994b4a
yuly <yuly@basovich.com>
2017-08-01 01:33:02 -0400

    Fixing env variables names to match OS format

--- src/main/scala/com/sysgears/example/config/Configuration.scala
+++ src/main/scala/com/sysgears/example/config/Configuration.scala
@@ -20,27 +20,27 @@ trait Configuration {
   lazy val servicePort = Try(config.getInt("service.port")).getOrElse(8080)
 
   /** Database host name/address. */
-  lazy val dbHost = Try(sys.env("db.host")).getOrElse(
+  lazy val dbHost = Try(sys.env("db_host")).getOrElse(
     Try(config.getString("db.host")).getOrElse("localhost")
   )
 
 
   /** Database host port number. */
-  lazy val dbPort = Try(sys.env("db.port")).getOrElse(
+  lazy val dbPort = Try(sys.env("db_port")).getOrElse(
     Try(config.getInt("db.port")).getOrElse(3306)
   )
 
   /** Service database name. */
-  lazy val dbName = Try(sys.env("db.name")).getOrElse(
+  lazy val dbName = Try(sys.env("db_name")).getOrElse(
     Try(config.getString("db.name")).getOrElse("rest")
   )
 
   /** User name used to access database. */
-  lazy val dbUser = Try(sys.env("db.user")).getOrElse(
+  lazy val dbUser = Try(sys.env("db_user")).getOrElse(
     Try(config.getString("db.user")).toOption.orNull
   )
   /** Password for specified user and database. */
-  lazy val dbPassword = Try(sys.env("db.password")).getOrElse(
+  lazy val dbPassword = Try(sys.env("db_password")).getOrElse(
     Try(config.getString("db.password")).toOption.orNull
   )
 }
